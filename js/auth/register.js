0785907bc6b0a19b01ba90746b3e14512b1409c3
DalaScript <nik.dalalishvili@gmail.com>
2025-04-21 00:08:46 +0400

    implement final version

---
+++ js/auth/register.js
@@ -0,0 +1,13 @@
+const API_URL = "http://localhost:5000";
+
+document.addEventListener("DOMContentLoaded", () => {
+    const registerForm = document.getElementById("registerForm");
+    const registerMessage = document.getElementById("registerMessage");
+
+    registerForm.addEventListener("submit", async (e) => {
+        e.preventDefault();
+
+        const username = document.getElementById("username").value;
+        
+    })
+})
